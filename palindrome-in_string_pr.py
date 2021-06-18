b9cd37f0e29a45a7d063069eca7f632f9e590dd5
P-Raj27 <85934814+P-Raj27@users.noreply.github.com>
2021-06-18 16:54:01 +0530

    Update palindrome-in_string_pr.py

--- palindrome-in_string_pr.py
+++ palindrome-in_string_pr.py
@@ -1,18 +1,21 @@
 # python-practice
+#this program is O(n^2)
 S=input()
 def check_palindrom(string):
+    '''This functions return true for palindrome string'''
     if(string==string[::-1]):
         return True
     else:
         return False
-upper=len(S)
-lower=0
-lst=[]
-while(upper!=lower):
-        while(upper!=lower):
-            if(check_palindrom(S[lower:upper])):
-                lst.append(S[lower:upper])
-            upper=upper-1
-        lower=lower+1
-        upper=len(S)
-print(set(lst))
+if __name__ == "__main__":
+    upper=len(S)
+    lower=0
+    lst=[]
+    while(upper!=lower):                    #Traversing through the string in all possible manner.
+            while(upper!=lower):
+                if(check_palindrom(S[lower:upper])):      
+                    lst.append(S[lower:upper]) 
+                upper=upper-1
+            lower=lower+1
+            upper=len(S)
+    print(set(lst))               #To Avoid Duplication
