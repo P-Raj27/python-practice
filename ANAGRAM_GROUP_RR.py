0772369f57afd8b8c2d208fab3cc5884fd0c5b9e
P-Raj27 <85934814+P-Raj27@users.noreply.github.com>
2021-06-30 23:56:40 +0530

    Create ANAGRAM_GROUP_RR.py

---
+++ ANAGRAM_GROUP_RR.py
@@ -0,0 +1,12 @@
+  
+from collections import defaultdict
+def Anagrams(words):
+    '''
+    words: list of words
+    return : list of group of anagram
+    '''
+    anag=defaultdict(list)
+	  for ele in words:
+		  k=tuple(sorted(ele))  # sorted the characters of the word and made it tuple --- this is our KEY
+		  anag[k].append(ele)   # if elements of words list match KEY then we store the word as VALUES
+	  return(list(anag.values()))
