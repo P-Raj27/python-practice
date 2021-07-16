899cf8fecba3b65723ebb57f8dde574573f79b14
P-Raj27 <85934814+P-Raj27@users.noreply.github.com>
2021-07-16 22:44:26 +0530

    Update 0s and 1s segregate.py

--- 0s and 1s segregate.py
+++ 0s and 1s segregate.py
@@ -8,17 +8,19 @@ flag_l=0
 flag_r=0
 decision=True
 while(decision==True):
-    if(lst[left]==0):        #traverses till it finds 0 at positions and if found fun flag is raised
+    if(lst[left]==0):        #traverses till it finds 0 at positions and if found one flag is raised
         left=left+1
     else:
         flag_l=1
-    if(lst[right]==1):      ##traverses till it finds 1at positions and if found fun flag is raised
+    if(lst[right]==1):      ##traverses till it finds 1 at positions and if found zero flag is raised
         right=right-1
     else:
         flag_r=1
     if(flag_r==1 and flag_l==1):       #if both flag is raised then swaping is done
         if(left<right):
             lst[left],lst[right]=lst[right],lst[left]
+            flag_l=0
+            flag_r=0
     if(left>right):                       #if left becomes greater than right that means work is done
         decision=False
 print(lst)
